f04ee701beb23e70e6d059157b828b0b3c95da5e
EriPanci <eri.panci@gmail.com>
2022-10-24 11:10:29 +0200

    init commit

---
+++ resources/scripts/app.js
@@ -0,0 +1,25 @@
+import { domReady } from "@roots/sage/client";
+
+/**
+ * app.main
+ */
+const main = async (err) => {
+  if (err) {
+    // handle hmr errors
+    console.error(err);
+  }
+
+  // application code
+  const condition = document.querySelector(`.foo`);
+  if (condition) {
+    await import("./components/test.js");
+  }
+};
+
+/**
+ * Initialize
+ *
+ * @see https://webpack.js.org/api/hot-module-replacement
+ */
+domReady(main);
+import.meta.webpackHot?.accept(main);
